b9128bf5b85c6cd1e3e415b2d42524e839e4119a
Mazettt <76044580+Mazettt@users.noreply.github.com>
2021-02-19 19:28:49 +0100

    3

--- decrypt.py
+++ decrypt.py
@@ -14,5 +14,18 @@ def decrypt(text,step):
             result += chr((ord(char) + s - 97) % 26 + 97) #minuscules
         return result
 
-message_chiffre1 = open("message-chiffre.txt","r")
-message_chiffre2 = open("message-chiffre","w")
+try:
+    f = open(sys.argv[1], 'r') #open message
+    f1 = open("message-dechiffre.txt", 'w') #open message
+
+    step = int(input("Entrez le nombre de décalage des lettres : "))
+    text = f. ()
+    f1. (encrypt(text, 26-step))
+    f.close()
+    f1.close()
+    print("Le message chiffré est disponible sous le nom message-dechiffre.txt")
+
+except Exception:
+    print("Le fichier n'est pas existant.")
+
+# PAS FINI
